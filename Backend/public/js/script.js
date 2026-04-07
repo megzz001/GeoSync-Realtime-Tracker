07f92b3a1204502d63f3d45d1b70ef7ebac168a2
Megha <meghagusain03@gmail.com>
2026-04-07 11:04:13 +0530

    Added marker at the current location

--- Backend/public/js/script.js
+++ Backend/public/js/script.js
@@ -1,12 +1,12 @@
 const socket = io();
 
-if(navigator.geolocation ){
-    navigator.geolocation.watchPosition(Position =>{
-        const {latitude, longitude} = Position.coords;
-        socket.emit("sendLocation", {latitude, longitude});
-    },(error) =>{
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(Position => {
+        const { latitude, longitude } = Position.coords;
+        socket.emit("send-location", { latitude, longitude });
+    }, (error) => {
         console.error("Error getting location: ", error);
-    },{
+    }, {
         enableHighAccuracy: true,
         timeout: 5000,
         maximumAge: 0 // No caching of location
@@ -19,6 +19,16 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-const marker = {
-    
+const markers = {
+
 }
+
+socket.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
+    map.setView([latitude, longitude], 16);
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+});
